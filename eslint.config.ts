6275e815ff8ed8883ea64f65bf41264fc1648e1b
Klemens Träger <traegerklemens@gmail.com>
2026-02-18 21:01:38 +0100

    add linting

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  },
+  ignores: [
+    '.nuxt/**',
+    '.output/**',
+    'coverage/**',
+    'server/db/migrations/**',
+    'darts_scorer/**',
+    'plans/**',
+  ],
+}, {
+  rules: {
+    'no-console': 'warn',
+  },
+})
